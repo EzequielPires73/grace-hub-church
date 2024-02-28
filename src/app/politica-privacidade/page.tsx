2a2cb61227659683dfa585f283b1455d0e82e3d4
Ezequiel Pires <ezequiel.pires0810@gmail.com>
2024-02-28 16:52:59 -0300

    Commit

---
+++ src/app/politica-privacidade/page.tsx
@@ -0,0 +1,47 @@
+export default function Page() {
+    return (
+        <div className="flex flex-col items-start w-full max-w-7xl mx-auto py-10">
+            <h1 className="text-3xl font-bold mb-4">Política de Privacidade</h1>
+            <div className="mb-4">
+                <p className="text-lg">Última atualização: 15 de fevereiro de 2024</p>
+            </div>
+            <div className="mb-4">
+                <p className="text-lg">A equipe por trás do aplicativo "Agenda Pastoral AD Catalão" está comprometida em proteger a sua privacidade. Esta Política de Privacidade descreve como suas informações pessoais são coletadas, usadas e compartilhadas quando você utiliza nosso aplicativo.</p>
+            </div>
+            <h2 className="text-2xl font-bold mb-2">Informações que Coletamos</h2>
+            <div className="mb-4">
+                <p className="text-lg">- Informações de Conta: Podemos coletar informações como nome, endereço de e-mail e outras informações de contato que você fornecer voluntariamente ao criar uma conta ou usar determinados recursos do aplicativo.</p>
+                <p className="text-lg">- Informações de Uso: Podemos coletar informações sobre como você utiliza o aplicativo, como as páginas que visita, as funcionalidades que utiliza e outras estatísticas de uso.</p>
+            </div>
+            <h2 className="text-2xl font-bold mb-2">Como Utilizamos Suas Informações</h2>
+            <div className="mb-4">
+                <p className="text-lg">As informações que coletamos podem ser utilizadas para:</p>
+                <ul className="list-disc pl-5">
+                    <li>Fornecer e manter o aplicativo;</li>
+                    <li>Personalizar sua experiência dentro do aplicativo;</li>
+                    <li>Enviar-lhe atualizações, notificações e outras comunicações relacionadas ao aplicativo;</li>
+                </ul>
+            </div>
+            <div className="mb-4">
+                <h2 className="text-2xl font-bold">Compartilhamento de Informações</h2>
+                <p className="text-lg">Não compartilhamos suas informações pessoais com terceiros, exceto quando necessário para oferecer suporte e manter o funcionamento do aplicativo ou quando exigido por lei.</p>
+            </div>
+            <div className="mb-4">
+                <h2 className="text-2xl font-bold">Segurança</h2>
+                <p className="text-lg">Valorizamos a segurança de suas informações pessoais e adotamos medidas razoáveis para protegê-las contra acesso não autorizado, uso indevido ou divulgação.</p>
+            </div>
+            <div className="mb-4">
+                <h2 className="text-2xl font-bold">Seus Direitos</h2>
+                <p className="text-lg">Você tem o direito de acessar, corrigir ou excluir suas informações pessoais a qualquer momento. Se desejar exercer esses direitos ou tiver alguma dúvida sobre esta Política de Privacidade, entre em contato conosco utilizando as informações fornecidas abaixo.</p>
+            </div>
+            <div className="mb-4">
+                <h2 className="text-2xl font-bold">Alterações a Esta Política de Privacidade</h2>
+                <p className="text-lg">Podemos atualizar esta Política de Privacidade de tempos em tempos. Recomendamos que você revise periodicamente esta página para estar ciente de quaisquer alterações. As alterações nesta Política de Privacidade são efetivas quando publicadas nesta página.</p>
+            </div>
+            <div className="mb-4">
+                <h2 className="text-2xl font-bold">Contato</h2>
+                <p className="text-lg">Se tiver alguma dúvida ou preocupação sobre esta Política de Privacidade ou sobre como tratamos suas informações pessoais, entre em contato conosco pelo e-mail [inserir e-mail de contato].</p>
+            </div>
+        </div>
+    )
+}
