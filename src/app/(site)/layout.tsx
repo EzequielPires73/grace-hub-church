e4ac7e59880c934e2772455b317594cdb0f3dab6
Ezequiel Pires <ezequiel.pires0810@gmail.com>
2024-03-14 13:22:39 -0300

    Commit

---
+++ src/app/(site)/layout.tsx
@@ -0,0 +1,19 @@
+import { HeaderSite } from "@/components/headers/header-site";
+import Image from "next/image";
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className="flex flex-col">
+            <HeaderSite />
+            {children}
+            <footer className="bg-gray-900 py-4 px-3 gap-2 flex flex-col justify-center items-center">
+                <Image src={'/assets/logo-white.png'} alt="" width={100} height={100} />
+                <span className="text-sm text-white text-center">2024 © AD Catalão - Todos os direitos de uso do conteúdo desse site são reservados.</span>
+            </footer>
+        </div>
+    );
+}
---
+++ src/components/headers/header-site.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import { FaWhatsapp } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaTwitch, FaTwitter, FaYoutube } from "react-icons/fa6";
+import { FiMenu } from "react-icons/fi";
+
+export function HeaderSite() {
+    return (
+        <header className="h-24">
+            <nav className="w-full max-w-7xl h-full mx-auto px-3 flex items-center justify-between max-md:justify-center relative">
+                <FiMenu size={32} className="text-gray-700 max-lg:absolute left-3" />
+                <Image src="/assets/logo-icon.svg" alt="Logo" width={96} height={64}/>
+                <ul className="text-gray-700 flex gap-4 max-lg:hidden">
+                    <li><FaTwitter size={18} /></li>
+                    <li><FaInstagram size={18} /></li>
+                    <li><FaFacebook size={18} /></li>
+                    <li><FaYoutube size={18} /></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
