64ff6abfd1ff49eda0fbfb87916b4aadd5c79b1e
EzequielPires <ezequiel.pires082000@gmail.com>
2024-05-09 11:20:37 -0300

    Commit

--- src/components/home-sections/home-section-youtube.tsx
+++ src/components/home-sections/home-section-youtube.tsx
@@ -9,7 +9,7 @@ export function HomeSectionYoutube() {
             <section className="bg-gray-900/80">
               <section className="h-full min-h-[400px] py-10 flex flex-col justify-center items-center gap-6 w-full max-w-7xl px-3 mx-auto">
                 <h2 className="text-xl font-semibold text-white text-center">CONHEÇA O CANAL DO YOUTUBE</h2>
-                <div className="flex justify-center max-md:flex-wrap gap-6">
+                <div className="flex flex-wrap justify-center max-md:flex-wrap gap-6">
                   <iframe width="360px" height="202px" src="https://www.youtube.com/embed/x22TixS8enE?si=fuFzgUmFVgoBlD0N" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
                   <iframe width="360px" height="202px" src="https://www.youtube.com/embed/hrjD804Auok?si=az4uHKD4V6Xn8hCt" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
                   <iframe width="360px" height="202px" src="https://www.youtube.com/embed/TcdnSo8GeSo?si=d9zyxn9ecAcaxbn0" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
