9cf2ec2c34b09d99a3aeb41b342a84c9c3b0ab26
EzequielPires <ezequiel.pires082000@gmail.com>
2024-05-20 11:40:15 -0300

    Commit

--- src/app/(site)/events/[id]/page.tsx
+++ src/app/(site)/events/[id]/page.tsx
@@ -18,9 +18,9 @@ export default async function NewsPage({ params }) {
     return (
         <div className="flex-1 flex flex-col gap-6 w-full max-w-[800px] px-3 mx-auto lg:py-6 mb-6">
             {event.image && <div className="w-full h-[248px] lg:h-[336px] relative rounded-lg overflow-hidden">
-                <Image src={process.env.NEXT_PUBLIC_URL_DEFAULT + event.image} alt="" fill objectFit="cover" />
+                <img src={process.env.NEXT_PUBLIC_URL_DEFAULT + event.image} alt="" /* fill objectFit="cover"  *//>
                 <div className="absolute top-0 left-0 right-0 bottom-0 backdrop-blur-lg"></div>
-                <Image src={process.env.NEXT_PUBLIC_URL_DEFAULT + event.image} alt="" fill objectFit="contain" />
+                <img src={process.env.NEXT_PUBLIC_URL_DEFAULT + event.image} alt="" /* fill objectFit="contain"  *//>
             </div>}
             <div className="w-full flex items-center justify-between gap-6">
                 <div className="flex flex-col gap-3">
