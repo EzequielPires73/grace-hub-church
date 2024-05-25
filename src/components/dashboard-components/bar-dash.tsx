92586f9635d0295917b05ab2f22df0f4ec0d8d38
Ezequiel Pires <ezequiel.pires0810@gmail.com>
2024-05-25 16:32:48 -0300

    Commit

--- src/components/dashboard-components/bar-dash.tsx
+++ src/components/dashboard-components/bar-dash.tsx
@@ -16,6 +16,9 @@ import {
 
 } from 'chart.js';
 import { roundToDecimalPlace } from '@/helpers/functions';
+import { ButtonPrimary } from '../buttons/button-primary';
+import { useEffect, useRef, useState } from 'react';
+import { FiArrowLeft } from 'react-icons/fi';
 
 ChartJS.register(
     CategoryScale,
@@ -50,31 +53,36 @@ export function BarDash({ results }) {
     }
 
     const data_bar: ChartData<'bar'> = {
-        labels,
+        labels: labels.slice(0, 15),
         datasets: [
             {
                 label: 'Esperado',
-                data: results.map((item, index) => item.expected),
+                data: results.slice(0, 15).map((item, index) => item.expected),
                 borderColor: 'rgb(255, 99, 132)',
                 backgroundColor: 'rgba(255, 99, 132)',
             },
             {
                 label: 'Presentes',
-                data: results.map((item, index) => item.attended),
+                data: results.slice(0, 15).map((item, index) => item.attended),
                 borderColor: '#2563eb',
                 backgroundColor: '#2563eb',
             },
         ],
     };
-
-    const data_bar_1: ChartData<'bar'> = {
-        labels,
+    const data_bar_2: ChartData<'bar'> = {
+        labels: labels.slice(15, 30),
         datasets: [
             {
-                label: 'Presença dos membros',
-                data: results.map(item => item.percentage),
+                label: 'Esperado',
+                data: results.slice(15, 30).map((item, index) => item.expected),
                 borderColor: 'rgb(255, 99, 132)',
                 backgroundColor: 'rgba(255, 99, 132)',
+            },
+            {
+                label: 'Presentes',
+                data: results.slice(15, 30).map((item, index) => item.attended),
+                borderColor: '#2563eb',
+                backgroundColor: '#2563eb',
             },
         ],
     };
@@ -90,6 +98,10 @@ export function BarDash({ results }) {
                 <Bar
                     options={options}
                     data={data_bar}
+                />
+                <Bar
+                    options={options}
+                    data={data_bar_2}
                 />
             </div>
             {/* <div className='relative flex flex-col gap-4 bg-white'>
@@ -107,6 +119,23 @@ export function BarDash({ results }) {
 }
 
 export function CustomPieChart({ item, index }) {
+    const [show, setShow] = useState(false);
+    const ref = useRef(null);
+
+    const handleOutsideClick = (event: any) => {
+        if (ref.current && !ref.current.contains(event.target)) {
+            setShow(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick);
+        };
+    }, []);
+
     const getValue = (value) => {
         if (value < 0) {
             return 0;
@@ -153,21 +182,48 @@ export function CustomPieChart({ item, index }) {
                 options={options}
                 data={data_bar}
             /> */}
-            <Pie
-                options={{
-                    ...options, plugins: {
-                        tooltip: {
-                            callbacks: {
-                                label: function (context) {
-                                    const percentage = context.dataset.data[context.dataIndex];
-                                    return `${getValue(percentage)}%`;
+            <div className='w-full max-w-[300px]'>
+                <Pie
+                    options={{
+                        ...options, plugins: {
+                            tooltip: {
+                                callbacks: {
+                                    label: function (context) {
+                                        const percentage = context.dataset.data[context.dataIndex];
+                                        return `${getValue(percentage)}%`;
+                                    }
                                 }
                             }
                         }
-                    }
-                }}
-                data={data_bar_1}
-            />
+                    }}
+                    data={data_bar_1}
+                />
+            </div>
+            <ButtonPrimary title='Ver membros presentes' onClick={() => setShow(true)} />
+            {show &&
+                <div className="fixed top-0 bottom-0 left-0 right-0 bg-black/50 flex items-center justify-center z-[9999]">
+                    <div className="bg-white w-full max-w-7xl rounded-md flex flex-col h-[80vh] max-lg:rounded-none" ref={ref}>
+                        <header className="h-16 bg-blue-500 rounded-t-md flex items-center justify-between px-3 max-lg:rounded-none">
+                            <div className="flex items-center gap-3">
+                                <button onClick={() => setShow(false)} className="h-10 w-10 flex items-center justify-center bg-blue-600 rounded-md font-semibold text-white">
+                                    <FiArrowLeft size={16} />
+                                </button>
+                                <span className="text-lg font-medium text-white">Membros Presentes {item.congregation.name}</span>
+                            </div>
+                        </header>
+                        <div className="p-3 flex-1 flex flex-col gap-4 overflow-auto">
+                            {item.data.map(item => (
+                                <div className='flex flex-col gap-1 border-b py-2'>
+                                    <span><strong>Nome:</strong> {item.name}</span>
+                                    <span><strong>Cargo:</strong> {item.position}</span>
+                                    <span><strong>CPF:</strong> {item.cpf}</span>
+                                    <span><strong>Telefone:</strong> {item.phone}</span>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
